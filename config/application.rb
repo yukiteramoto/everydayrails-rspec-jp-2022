1169d495792b8f3874ff20545556ac3fff605183
yuki teramoto <yuki_teramoto@kidsline.co.jp>
2023-02-24 23:21:21 +0900

    02-setup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    cofnig.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      g.factory_bot false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
