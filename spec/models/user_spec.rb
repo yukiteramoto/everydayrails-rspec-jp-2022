5e94455621456ca342dc3e8e8b16d5042bf99154
yuki teramoto <yuki_teramoto@kidsline.co.jp>
2023-02-26 23:47:13 +0900

    add validation spec to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aron",
+      last_name: "Sumner",
+      email:     "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a first name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user = User.new(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+  it "returns a user's full name as a string"
 end
