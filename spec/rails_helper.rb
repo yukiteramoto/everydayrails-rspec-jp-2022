f8f06c682e878882d59c08f18e8d91e392cdf717
yuki teramoto <yuki_teramoto@kidsline.co.jp>
2023-03-17 23:44:10 +0900

    add request spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, tyep: :request
 end
