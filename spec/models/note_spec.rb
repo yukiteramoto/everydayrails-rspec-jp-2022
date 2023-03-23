2a8a461b7591168a7e093950ce52482cc98d9251
yuki teramoto <yuki_teramoto@kidsline.co.jp>
2023-03-24 01:00:54 +0900

    テスト修正

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -6,11 +6,10 @@ RSpec.describe Note, type: :model do
 
   it "is valid with a user, project, and message" do
     note = Note.new(
-      message: "This is a sample note",
+      message: "This is a sample note.",
       user: user,
       project: project,
     )
-
     expect(note).to be_valid
   end
 
@@ -18,6 +17,11 @@ RSpec.describe Note, type: :model do
     note = Note.new(message: nil)
     note.valid?
     expect(note.errors[:message]).to include("can't be blank")
+  end
+
+  it 'has one attached attachment' do
+    note = FactoryBot.create(:note, :with_attachment)
+    expect(note.attachment).to be_attached
   end
 
   describe "search message for a term" do
@@ -46,15 +50,24 @@ RSpec.describe Note, type: :model do
     }
 
     context "when a match is found" do
-      it "returns note that match the search term" do
+      it "returns notes that match the search term" do
         expect(Note.search("first")).to include(note1, note3)
+        expect(Note.search("first")).to_not include(note2)
       end
     end
 
     context "when no match is found" do
       it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
+  end
+
+  it "delegates name to the user who created it" do
+    user = instance_double("User", name: "Fake User")
+    note = Note.new
+    allow(note).to receive(:user).and_return(user)
+    expect(note.user_name).to eq "Fake User"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,40 +5,39 @@ RSpec.describe User, type: :model do
     expect(FactoryBot.build(:user)).to be_valid
   end
 
-  it "is valid with a first name, last name, email, and password" do
+  it "is valid with a first name, last name and email, and password" do
     user = User.new(
-      first_name: "Aron",
-      last_name: "Sumner",
-      email:     "tester@example.com",
+      first_name: "Aaron",
+      last_name:  "Sumner",
+      email:      "tester@example.com",
       password:   "dottle-nouveau-pavilion-tights-furze",
     )
     expect(user).to be_valid
   end
 
-  it "is invalid without a first name" do
-    user = FactoryBot.build(:user, first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include("can't be blank")
-  end
-  it "is invalid without a last name" do
-    user = FactoryBot.build(:user, last_name: nil)
-    user.valid?
-    expect(user.errors[:last_name]).to include("can't be blank")
-  end
-  it "is invalid without an email address" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-  it "is invalid with a duplicate email address" do
-    FactoryBot.create(:user, email: "aaron@example.com")
-    user = FactoryBot.build(:user, email: "aaron@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
   it "returns a user's full name as a string" do
     user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
-
     expect(user.name).to eq "John Doe"
+  end
+
+  it "sends a welcome email on account creation" do
+    allow(UserMailer).to \
+      receive_message_chain(:welcome_email, :deliver_later)
+    user = FactoryBot.create(:user)
+    expect(UserMailer).to have_received(:welcome_email).with(user)
+  end
+
+  it "performs geocoding", vcr: true do
+    user = FactoryBot.create(:user, last_sign_in_ip: "161.185.207.20")
+    expect {
+      user.geocode
+    }.to change(user, :location).
+      from(nil).
+      to("Brooklyn, New York, US")
   end
 end
